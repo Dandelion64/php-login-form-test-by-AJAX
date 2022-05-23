c00cbce4754fcce4375e3565672962093ecce1db
Dandelion64 <coolpillow0308@gmail.com>
2022-05-23 22:11:28 +0800

    backend validator

---
+++ validator.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+// 這邊只是為了 Demo
+// 一般不會寫死在程式碼中 不方便新增用戶
+$users = [
+    'waiting' => [
+        'password' => '1234',
+        'nickname' => '等待',
+    ],
+    'slumdar' => [
+        'password' => '5678',
+        'nickname' => '史朗德',
+    ],
+];
+
+$output = [
+    'postData' => $_POST, // 除錯用的欄位
+];
+
+if (isset($_POST['account'])) {
+    // echo json_encode($_POST);
+    // exit; // 立刻中止 php 程式執行
+    // die(); // 和 exit; 很像
+
+    // empty() 如果是空字串 空數值 空陣列 或未設定 true
+    if (!empty($_POST['account']) and !empty($_POST['password'])) {
+        if (!empty($users[$_POST['account']])) {
+            if ($_POST['password'] === $users[$_POST['account']]['password']) {
+                // 登入成功
+                $output['msg'] = '登入成功';
+            } else {
+                // 帳號正確但密碼錯誤
+                $output['msg'] = '密碼錯誤';
+            }
+        } else {
+            // 帳號錯誤
+            $output['msg'] = '帳號錯誤';
+        }
+
+        echo json_encode($output['msg'], JSON_UNESCAPED_UNICODE);
+        exit;
+    } else {
+        $output['msg'] = '帳號不存在';
+        echo json_encode($output['msg'], JSON_UNESCAPED_UNICODE);
+        exit;
+    }
+}
